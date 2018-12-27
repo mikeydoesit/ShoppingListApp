6524692e1f8f5aec0493ecb915b9fe532d7f261b
mikeydoesit <michaelldninc@gmail.com>
2018-12-27 21:22:24 +0000

    Add ingredient logic

--- main.js
+++ main.js
@@ -1,29 +1,73 @@
+// input
+
+
+// List variables
 var ul = document.getElementById('list')
 var li;
 
-
+// Buttons
 var addIngredientButton = document.getElementById('add')
 var removeIngredientButton = document.getElementById('remove')
 var clearListButton = document.getElementById('clear')
 
+// Event Listeners
 addIngredientButton.addEventListener('click', addIngredient)
 removeIngredientButton.addEventListener('click', removeIngredient)
 clearListButton.addEventListener('click', clearList)
 
-
+// Functions
+//Add
 function addIngredient() {
-  console.log('Added item');
+  var input = document.getElementById('input')
+  var newIngredient = input.value
+  ul = document.getElementById('list')
+  var textnode = document.createTextNode(newIngredient)
+
+  if (newIngredient === '') {
+    // return false;
+    var parentNode = document.querySelector('.controls')
+    var inputWarning = document.createElement('p')
+    var inputWarningText = document.createTextNode('Enter an ingredient')
+    inputWarning.appendChild(inputWarningText)
+    parentNode.insertBefore(inputWarning, input)
+  } else {
+    // create li
+    li = document.createElement('li');
+
+    // create checkbox
+    var checkbox = document.createElement('input')
+    checkbox.type = 'checkbox';
+    checkbox.setAttribute('id', 'check');
+
+    // create label
+    var label = document.createElement('label')
+
+    //add these elements on page
+    ul.appendChild(label)
+    li.appendChild(checkbox)
+    label.appendChild(textnode)
+    li.appendChild(label)
+    ul.insertBefore(li, ul.childNodes[0])
+
+    setTimeout(() => {
+      li.className = 'visual';
+    }, 2);
+
+    input.value = ''
+  }
 }
 
+// Remove selected ingredients
 function removeIngredient() {
   li = ul.children
 for (let i = 0; i < li.length; i++) {
   while (li[i] && li[i].children[0].checked) {
     ul.removeChild(li[i])
+    }
   }
- }
 }
 
+// Clear the whole List
 function clearList() {
   li = ul.children
 
@@ -31,7 +75,5 @@ function clearList() {
     while (li[i] && li[i].children[0]) {
       ul.removeChild(li[i])
     }
-
   }
-
 }
